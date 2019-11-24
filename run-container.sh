6fa6bb2d3c50324c237dbcef5d4cc9f67d9abe64
ffffansz <branchvan379@gmail.com>
2019-11-25 00:50:09 +0800

    clean

--- run-container.sh
+++ run-container.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
-docker run -itd --rm -p 8080:8080 --name 2019semdoc \
---mount type=bind,source=/root/sites/2019semdoc,target=/site \
-hugo:latest hugo server --minify --gc -p 8080 --bind 0.0.0.0 --baseUrl=http://172.81.239.128:8080
+SITE_NAME=2019semdoc
+PORT=8080
+
+# 备案之前只能先用IP了
+DOMAIN=172.81.239.128
+docker run -itd --rm -p ${PORT}:${PORT} --name hugo-${SITE_NAME} \
+--mount type=bind,source=/root/sites/${SITE_NAME},target=/site \
+hugo:latest hugo server --minify --gc -p ${PORT} --bind 0.0.0.0 --baseUrl=http://${DOMAIN}:${PORT}
