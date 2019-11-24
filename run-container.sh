08fab6a1802b2c925c117295f969bf814993ac50
ffffansz <branchvan379@gmail.com>
2019-11-24 20:58:04 +0800

    update

--- run-container.sh
+++ run-container.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-docker run -it --rm -p 8080:8080 --name 2019semdoc \
+docker run -itd --rm -p 8080:8080 --name 2019semdoc \
 --mount type=bind,source=/root/sites/2019semdoc,target=/site \
-hugo:latest hugo server --minify --gc -p 8080 --bind 0.0.0.0
+hugo:latest hugo server --minify --gc -p 8080 --bind 0.0.0.0 --baseUrl=http://172.81.239.128:8080
